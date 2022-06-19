c0055efece2838141e8f5eafb24d687d1e873995
Goitbit <matej.goitka@seznam.cz>
2022-06-19 23:42:34 +0200

    added slideshow
    -added slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,18 +1,49 @@
 window.addEventListener('DOMContentLoaded', function(){
-imgEm = document.getElementById("slajdimg");
-var slideIndex = 2;
+var imgEm = document.getElementById("slajdimg");
+var slideIndex = 0;
+var leftArrow = document.getElementById("leftarrow");
+var rightArrow = document.getElementById("rightarrow");
+
+
+slides = [
+    {
+        src: "img1.jpg"
+    },
+    {
+        src: "img2.jpg"
+    },
+    {
+        src: "img3.jpg"
+    }
+]    
     
-switch(slideIndex){
-    case 0:
-    imgEm.src = "img1.jpg"
-    break;
-    case 1:
-    imgEm.src = "img2.jpg"
-    break;
-    case 2: 
-    imgEm.src = "img3.jpg"
-    break; 
+        // always checking if the element is clicked, if so, do alert('hello')
+rightArrow.addEventListener("click", () => {
+    if (slideIndex < slides.length - 1) {
+    slideIndex += 1;
+    changeImg();
+    } else {
+    slideIndex = 0;
+    changeImg();
+    }
+});
+    
+        // always checking if the element is clicked, if so, do alert('hello')
+leftArrow.addEventListener("click", () => {
+    if (slideIndex > 0) {
+    slideIndex -= 1;
+    changeImg();
+    } else {
+    slideIndex = slides.length - 1;
+    changeImg();
+    }
+});    
+    
+    
+function changeImg(){
+imgEm.src = slides[slideIndex].src;
 }
     
+changeImg();    
 });
             
