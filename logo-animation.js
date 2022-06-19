2608a408a762a32224087cced22ee7db235b8780
Goitbit <66004264+Goitbit@users.noreply.github.com>
2022-06-19 22:22:46 +0200

    Add files via upload

---
+++ logo-animation.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+var logoId = document.getElementById("logo");
+    
+setInterval(function(){
+if (logoId.innerText == "Matěj Goitka|"){
+    logoId.innerText = "Matěj Goitka";
+} else {
+    logoId.innerText = "Matěj Goitka|";
+}
+}, 450)    
+    
+}
---
+++ slideshow.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function(){
+imgEm = document.getElementById("slajdimg");
+var slideIndex = 2;
+    
+switch(slideIndex){
+    case 0:
+    imgEm.src = "img1.jpg"
+    break;
+    case 1:
+    imgEm.src = "img2.jpg"
+    break;
+    case 2: 
+    imgEm.src = "img3.jpg"
+    break; 
+}
+    
+});
+            
