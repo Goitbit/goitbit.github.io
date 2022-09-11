67ee931b2df2ed004f70da572c1c1b69100e6663
Goitbit <matej.goitka@seznam.cz>
2022-09-11 16:10:37 +0200

    Saving darkmode in local Storage

--- darkmode.js
+++ darkmode.js
@@ -1,12 +1,27 @@
-const toggleSwitch = document.getElementById('darkmodeSwitch');
+window.onload=function(){
+const toggleSwitch = document.getElementById("darkmodeSwitch");
+var theme = localStorage.getItem('data-theme');    
+    
+   if(theme=='light'){
+     document.documentElement.setAttribute('data-theme', 'light');
+   }else if(theme==''){
+     document.documentElement.setAttribute('data-theme', 'dark');
+   }else if(theme=='dark'){
+     document.documentElement.setAttribute('data-theme' , 'dark');
+   }
+ }
+
+
 
 toggleSwitch.onclick = function() {
-    if (toggleSwitch.innerText == "Světlý režim") {
+    if (toggleSwitch.innerText == "Světlý režim") { //Přepni do světlého režimu
         toggleSwitch.innerText = "Tmavý režim";        
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('data-theme', 'light');          
     }
-    else {
+    else { //Přepni do tmavého režimu
         toggleSwitch.innerText = "Světlý režim";
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('data-theme', 'dark');              
     }    
 }
